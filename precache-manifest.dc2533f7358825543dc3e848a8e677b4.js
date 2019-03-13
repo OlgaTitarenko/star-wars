03eb834252ab30ec9d1a0c233b010e179f06578c
титаренко <kizuneworld@gmail.com>
2019-03-13 15:19:25 +0200

    Updates

---
+++ precache-manifest.dc2533f7358825543dc3e848a8e677b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e03336767f8793a3f55",
+    "url": "/star-wars/static/js/runtime~main.1500f48b.js"
+  },
+  {
+    "revision": "fe9ec0b0eb9a0c76e13b",
+    "url": "/star-wars/static/js/main.228144fd.chunk.js"
+  },
+  {
+    "revision": "172a57de48b204a4c7a9",
+    "url": "/star-wars/static/js/2.9b774502.chunk.js"
+  },
+  {
+    "revision": "fe9ec0b0eb9a0c76e13b",
+    "url": "/star-wars/static/css/main.51be42c0.chunk.css"
+  },
+  {
+    "revision": "4a87a40767f77e229e5bab53c712ec90",
+    "url": "/star-wars/index.html"
+  }
+];
